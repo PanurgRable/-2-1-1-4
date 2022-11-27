b48c942785bd42f88ea5c0488b46afd418c3837e
PanurgRable <112486624+PanurgRable@users.noreply.github.com>
2022-11-27 10:42:41 +0300

    add programm

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/mydbTest";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root000000!";
+
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName(DB_DRIVER);
+            con = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connection OK");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection Error");
+        }
+        return con;
+    }
 }
